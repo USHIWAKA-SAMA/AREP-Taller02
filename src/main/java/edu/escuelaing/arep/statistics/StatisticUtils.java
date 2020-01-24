c8f9ea85afd4d8f57ec49c52fbe15c7b184468cf
Julian <julian.benitez@mail.escuelaing.edu.co>
2020-01-24 12:57:36 -0500

    LinkedList/Statistic classes

---
+++ src/main/java/edu/escuelaing/arep/statistics/StatisticUtils.java
@@ -0,0 +1,39 @@
+package edu.escuelaing.arep.statistics;
+
+import java.util.List;
+
+/**
+ * Class for utility statistics methods
+ */
+public class StatisticUtils {
+
+    /**
+     * Calculates the mean of a set of real numbers
+     * @param numbers List of real numbers
+     * @return The mean of the given list
+     */
+    public static double calculateMean(List<Double> numbers){
+        int total = numbers.size();
+        double sum = 0;
+        for (Double num : numbers){
+            sum += num;
+        }
+        return sum/total;
+    }
+
+    /**
+     * Calculates the standard deviation of a set of real numbers
+     * @param numbers List of real numbers
+     * @return The standard deviation of the given list
+     */
+    public static double calculateStandardDeviation(List<Double> numbers){
+        double total = numbers.size();
+        double avg = calculateMean(numbers);
+        double numMinusAvgSquared = 0;
+        for (Double num : numbers) {
+            numMinusAvgSquared += Math.pow(num - avg, 2);
+        }
+        return Math.sqrt(numMinusAvgSquared/(total-1));
+    }
+
+}
