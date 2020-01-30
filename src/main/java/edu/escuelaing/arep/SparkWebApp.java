ba12b9c9b11dd90dcef61f4e7cf90c0c3e986f22
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2020-01-29 20:54:16 -0500

    Tests / README / Javadocs

--- src/main/java/edu/escuelaing/arep/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/SparkWebApp.java
@@ -9,8 +9,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import static spark.Spark.*;
+
 import org.decimal4j.util.DoubleRounder;
 
+/**
+ * Spark web application class for calculating the mean and standard deviation of a set of real numbers.
+ */
 public class SparkWebApp {
 
     public static void main(String[] args) {
@@ -21,11 +25,12 @@ public class SparkWebApp {
 
     /**
      * Gets the index page of the application
+     *
      * @param req Provides information about the HTTP request
      * @param res Provides information about the HTTP response
      * @return The index in html page
      */
-    private static String indexPage(Request req, Response res){
+    private static String indexPage(Request req, Response res) {
         return "<html>\n" +
                 "<head>\n" +
                 "  <title>StatisticsApp</title>\n" +
@@ -46,19 +51,19 @@ public class SparkWebApp {
 
     /**
      * Gets the calculation page of the application
+     *
      * @param req Provides information about the HTTP request
      * @param res Provides information about the HTTP response
      * @return The calculation page with the mean and standard deviation of the set of data
      */
-    private static String calculatePage(Request req, Response res){
+    private static String calculatePage(Request req, Response res) {
         String numbersParam = req.queryParams("numbers");
         String[] numbers = numbersParam.split("\\s*(,)\\s*");
         List<Double> linkedList = new LinkedList<>();
         for (String num : numbers) {
-            try{
+            try {
                 linkedList.add(Double.parseDouble(num));
-            }
-            catch (NumberFormatException e){
+            } catch (NumberFormatException e) {
                 return "<html>\n" +
                         "<head>\n" +
                         "  <title>StatisticsApp</title>\n" +
@@ -84,15 +89,15 @@ public class SparkWebApp {
                 "<body style=\"background-color:#d9ffb3\">\n" +
                 "  <div style=\"margin:auto\">\n" +
                 "    <h1>Mean and Standard Deviation Calculator</h1>\n" +
-                "    The results for the data set <b>"+ Arrays.toString(numbers)+"</b>  are:\n" +
+                "    The results for the data set <b>" + Arrays.toString(numbers) + "</b>  are:\n" +
                 "    <table style=\"width:25%;border: 2px solid black\">\n" +
                 "      <tr >\n" +
                 "        <th style=\"border:1px solid black\">Mean</th>\n" +
                 "        <th style=\"border:1px solid black\">Std. Dev</th>\n" +
                 "      </tr>\n" +
                 "      <tr>\n" +
-                "        <td align=\"center\" style=\"border:1px solid black\">"+DoubleRounder.round(mean, 2)+"</td>\n" +
-                "        <td align=\"center\" style=\"border:1px solid black\">"+DoubleRounder.round(std, 2)+"</td>\n" +
+                "        <td align=\"center\" style=\"border:1px solid black\">" + DoubleRounder.round(mean, 2) + "</td>\n" +
+                "        <td align=\"center\" style=\"border:1px solid black\">" + DoubleRounder.round(std, 2) + "</td>\n" +
                 "    </table>\n" +
                 "    <br/>\n" +
                 "    <form action=\"/\">\n" +
@@ -106,6 +111,7 @@ public class SparkWebApp {
     /**
      * This method reads the default port as specified by the PORT variable in
      * the environment.
+     *
      * @return The port variable if set, else 4567 as default
      */
     static int getPort() {
